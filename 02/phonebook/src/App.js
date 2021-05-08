0b8809c1e2609886dd70e08652d8ee5bd6a4276b
Ville Valkila <aa2301@student.jamk.fi>
2021-05-08 20:28:03 +0300

    2.7 tehty

--- 02/phonebook/src/App.js
+++ 02/phonebook/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
   const [ persons, setPersons] = useState([
@@ -9,26 +10,41 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      id: persons.length + 1
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName("")
+    if (!(persons.some((person) => person.name === newName))) {
+      setPersons(persons.concat(personObject))
+      setNewName("")
+    } else {
+      window.alert(`${newName} is already added to phonebook`)
+    }
+  }
+
+  const handleFormChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={ addName }>
         <div>
-          name: <input />
+          name: <input 
+                  value={ newName } 
+                  onChange={ handleFormChange }
+                />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submt">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+          <Person key={person.id} person={person} />
+        )}
     </div>
   )
 
---
+++ 02/phonebook/src/components/Person.js
@@ -0,0 +1,7 @@
+const Person = ({ person }) => (
+    <p>
+        {person.name}
+    </p>
+)
+
+export default Person
